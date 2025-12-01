188105473f1117bbd7ba2d208b1b46ae090e525b
Kostya Igonin <Igoninqw@gmail.com>
2025-12-01 22:33:15 +0500

    refresh

--- homework/urls.py
+++ homework/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
 from homework.views import product_list, product_detail
 from homework.apps import HomeworkConfig
-
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = HomeworkConfig.name
 
 urlpatterns = [
-    path('', product_list),
-    path('homework/<int:pk>/',product_detail,name= 'product_detail')
+    path('', product_list, name= 'product_list'),
+    path('homework/<int:pk>/',product_detail, name= 'product_detail')
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
